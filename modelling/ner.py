de45b11b2b4481a72c3126434f59f0bf358d7181
Protik <mostafa.m.jalal@student.uts.edu.au>
2023-05-14 17:51:17 +1000

    bug fix try 5 ner.py

--- modelling/ner.py
+++ modelling/ner.py
@@ -11,11 +11,11 @@ def install_model(model):
 
 def load_model():
     try:
-        nlp = spacy.load('en_core_web_sm')
+        nlp = spacy.load('en')
     except OSError:
         st.write('Downloading language model... this may take a while')
         install_model('en_core_web_sm')
-        nlp = spacy.load('en_core_web_sm')
+        nlp = spacy.load('en')
     return nlp
 
 def get_ner(df, column, dataframe):
